cf06430c698ab4276c131b1163b8525b4fe56646
va0ck <120220068+va0ck@users.noreply.github.com>
2025-01-09 02:44:56 -0500

    DeveloperFinalMarquee

--- src/pages/HomeComponents/DevelopersMarquee.tsx
+++ src/pages/HomeComponents/DevelopersMarquee.tsx
@@ -8,99 +8,108 @@ import PlaceHolder from "../../assets/images/placeholder-1024x1024.png"
 
 interface DEVELOPERS_BACKGROUNDS_TABLE {
     id: number,
-    bgStyle: string
+    bgStyle: string,
+    invertTextColor?: boolean
+    backwardsInvertColor?: boolean
 }
 const DEVELOPERS_SET: DEVELOPERS_BACKGROUNDS_TABLE[] = [
     //Favorites
     {
         id: 35123490, //ZELNUS [Favorite Main Developer!]
-        bgStyle: 'bg-blue-950 hover:bg-blue-850'
+        bgStyle: 'bg-blue-950 hover:bg-blue-700'
     },
     {
         id: 4545223, //UNROOT [Favorite Ui Developer!]
-        bgStyle: 'bg-blue-950 hover:bg-blue-850'
+        bgStyle: 'bg-zinc-950 hover:bg-yellow-500'
     },
     {
         id: 16574047, //TheEpicTwin [Favorite CFrame Developer!
-        bgStyle: 'bg-blue-950 hover:bg-blue-850'
+        bgStyle: 'bg-rose-950 hover:bg-rose-800'
     },
 
     //Cool Developers
     {
         id: 4397833, //Quenty [Nevermore Developer!]
-        bgStyle: 'bg-blue-950 hover:bg-blue-850'
+        bgStyle: 'bg-green-800 hover:bg-green-500'
     },
     {
         id: 83348, //Osyris [Roblox Typescript!!!]
-        bgStyle: 'bg-blue-950 hover:bg-blue-850'
+        bgStyle: 'bg-red-900 hover:bg-red-600'
     },
     {
         id: 68136726, //JohnnyMorganz [LuaU Language Server!]
-        bgStyle: 'bg-blue-950 hover:bg-blue-850'
+        bgStyle: 'bg-cyan-900 hover:bg-cyan-600'
     },
 
     //Really Cool Developers
     {
         id: 308165, //sleitnick [GOT ME INTO OSS WITH KNIT!!! :D]
-        bgStyle: 'bg-blue-950 hover:bg-blue-850'
+        bgStyle: 'bg-black hover:bg-red-600',
+        invertTextColor: true
     },
     {
         id: 56140227, //Kampfkarren [SUPER DUPER HELPFUL REACT MAN!]
-        bgStyle: 'bg-blue-950 hover:bg-blue-850'
+        bgStyle: 'bg-red-500 hover:bg-blue-500'
     },
     {
         id: 74972628, //Fluorlite [SUPER DUPER COOL BUILDER!!!]
-        bgStyle: 'bg-blue-950 hover:bg-blue-850'
+        bgStyle: 'bg-white hover:bg-rose-400',
+        backwardsInvertColor: true
     },
 
     //Awesome Scripters
     {
         id: 18659509, //hlelo_wolrd [Networking Module inspo! :O]
-        bgStyle: 'bg-blue-950 hover:bg-blue-850'
+        bgStyle: 'bg-black hover:bg-white',
+        invertTextColor: true
     },
     {
         id: 2155311, //EgoMoose [Made Viewport portals! :O]
-        bgStyle: 'bg-blue-950 hover:bg-blue-850'
+        bgStyle: 'bg-orange-700 hover:bg-orange-400'
     },
     {
         id: 51635158, //lrogerorrit [Raycast and CFrame man! :O]
-        bgStyle: 'bg-blue-950 hover:bg-blue-850'
+        bgStyle: 'bg-slate-600 hover:bg-teal-500'
     },
     {
         id: 25691148, //TheNexusAvenger [Auto Datastore Removal & Wrap Uis! :O]
-        bgStyle: 'bg-blue-950 hover:bg-blue-850'
+        bgStyle: 'bg-sky-800 hover:bg-sky-400',
+        invertTextColor: true
     },
 
-    //Code @ Roblox
+    //Code @ Roblox [FINISH EDIT EVERYTHING BELOW HERE.]
     {
         id: 2231221, //TheGamer101 [Code @ Roblox which allowed me to learn springs!!]
-        bgStyle: 'bg-blue-950 hover:bg-blue-850'
+        bgStyle: 'bg-black hover:bg-white',
+        invertTextColor: true
     },
     {
         id: 1628829780, //MintyUltra [Code samples @ Roblox!]
-        bgStyle: 'bg-blue-950 hover:bg-blue-850'
+        bgStyle: 'bg-gray-300 hover:bg-black',
+        backwardsInvertColor: true,
     },
     {
         id: 1569334068, //DevelopmentDeadline [Code sampels @ Roblox!]
-        bgStyle: 'bg-blue-950 hover:bg-blue-850'
+        bgStyle: 'bg-rose-500 hover:bg-gray-300',
+        invertTextColor: true
     },
 
     //Awesome developers.
     {
         id: 1741242, //Den_S [No devlogs but cool developer for TPT2!!]
-        bgStyle: 'bg-blue-950 hover:bg-blue-850'
+        bgStyle: 'bg-blue-500 hover:bg-cyan-500'
     },
     {
         id: 80484191, //Szymon227G [Created GRACE and awesome devlogs!!]
-        bgStyle: 'bg-blue-950 hover:bg-blue-850'
+        bgStyle: 'bg-neutral-950 hover:bg-yellow-600'
     },
     {
         id: 2040318, //ChampionBuilder [Creating cool games! Fusion UI!]
-        bgStyle: 'bg-blue-950 hover:bg-blue-850'
+        bgStyle: 'bg-blue-700 hover:bg-red-500'
     },
     {
         id: 48016706, //NohahDev [Creating cool games! Fusion UI!]
-        bgStyle: 'bg-blue-950 hover:bg-blue-850'
+        bgStyle: 'bg-green-900 hover:bg-blue-500'
     },
 ]
 
@@ -109,7 +118,7 @@ interface DEVELOPERS_FETCHED_TABLE {
     username: string,
     rblxLink: string,
     imgFetched: string,
-    cardBgStyle?: string
+    cardBgStyle?: string,
 }
 const DEVELOPERS_FETCHED :DEVELOPERS_FETCHED_TABLE[] = []
 let hasRun = false;
@@ -149,6 +158,16 @@ export function DevelopersMarqueeFetchData(){
                 getDeveloperUserProfile.then((developerUserFetchedData: robloxUserApi) => {
                     const ifDeveloperAlreadyExist = valueExistsInNestedArray(DEVELOPERS_FETCHED, developerUserFetchedData.id);
                     const developerCardStyle = DEVELOPERS_SET.find((item) => item.id === gottenDeveloperTable.targetId)?.bgStyle;
+                    const ifInvertTextColor = DEVELOPERS_SET.find((item) => item.id === gottenDeveloperTable.targetId)?.invertTextColor;
+                    const ifInvertTextColor2 = DEVELOPERS_SET.find((item) => item.id === gottenDeveloperTable.targetId)?.backwardsInvertColor;
+                    let defaultNoInvertText = '';
+
+                    if (ifInvertTextColor){
+                        defaultNoInvertText = 'hover:text-black'
+                    }
+                    if (ifInvertTextColor2){
+                        defaultNoInvertText = 'text-black hover:text-white'
+                    }
 
                     if (ifDeveloperAlreadyExist == false){
                         const newAppendTable: DEVELOPERS_FETCHED_TABLE = {
@@ -156,7 +175,7 @@ export function DevelopersMarqueeFetchData(){
                             username: developerUserFetchedData.name,
                             rblxLink: `https://www.roblox.com/users/${gottenDeveloperTable.targetId}`,
                             imgFetched: gottenDeveloperTable.imageUrl,
-                            cardBgStyle: developerCardStyle
+                            cardBgStyle: "relative overflow-hidden m-2 w-80 h-56 hover:-translate-y-2 duration-300 rounded-3xl cursor-pointer text-2xl font-bold "+developerCardStyle+" "+defaultNoInvertText,
                         }
     
                         DEVELOPERS_FETCHED.push(newAppendTable);
@@ -169,15 +188,20 @@ export function DevelopersMarqueeFetchData(){
     }).catch((error) => {
         console.error('Promise rejected [DevelopersThumbnailsPromsie] with error: ' + error)
     })
+
+    if (DEVELOPERS_FETCHED.length >= 1){
+        return true;
+    } else {
+        return false;
+    }
 }
 
 export function DevelopersMarquee(){
-    //Grab data here and not in main Marquee. [TO AVOID LIKE MULTIPLE FETCHING EVERY RE-RENDER.
-
+    //Original 20 speed.
     return <Marquee speed={20} gradient gradientColor="rgb(3 7 18)" direction="right" autoFill className="overflow-hidden">
         {DEVELOPERS_FETCHED.map((getValue, getKey) => (
             <a href={getValue.rblxLink} key={getKey} target='_blank'>
-                <div className="relative overflow-hidden m-2 w-80 h-56 hover:-translate-y-2 duration-300 rounded-3xl cursor-pointer text-2xl font-bold bg-blue-950">
+                <div className={getValue.cardBgStyle}>
                     <img src={getValue.imgFetched} className="flex w-60 mt-0 -ml-6"/>
                     <h1 className="z-10 flex absolute bottom-0 pl-4 pb-3">@{getValue.username}</h1>
                 </div>
