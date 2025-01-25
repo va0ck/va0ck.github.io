cb0495f87aecbfe53b877627c80d12c2fa29ccff
va0ck <120220068+va0ck@users.noreply.github.com>
2025-01-24 23:02:34 -0500

    RouterChange

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,26 @@
 import './App.css'
-import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom';
 
-//Components
-import Loader from './components/Loader';
-
 import { Home } from './pages/Home';
-
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import { Works } from './pages/Works';
 import { Tools } from './pages/Tools';
 
 function App() {
-  const [loadingSpinner] = useState(false);
-
   return (
     <>
-      {loadingSpinner ? 
-      (<Loader/>) : 
-      (<>
-        <Navbar />
+      <Navbar />
 
-        <div className='pt-14'>
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path='works' element={<Works />} />
-            <Route path='tools' element={<Tools />} />
-          </Routes>
-        </div>
+      <div className='pt-14'>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path='/works' element={<Works />} />
+          <Route path='/tools' element={<Tools />} />
+        </Routes>
+      </div>
 
-        <Footer/>
-      </>)}
+      <Footer/>
     </>
   )
 }
